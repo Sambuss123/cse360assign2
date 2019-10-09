3bf944638640e7934b42300dae3f052d1a84372d
Sam Buss <sjbuss@asu.edu>
2019-10-09 10:47:36 -0700

    final version

--- addingMachine.java
+++ addingMachine.java
@@ -3,29 +3,32 @@ package cse360assign2;
 public class addingMachine {
 
 	private int total;
+	private String input = "0";
 	
 	public void AddingMachine () {
 		total = 0;  // not needed - included for clarity}
 	}
 	
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 	public void add (int value) {
-		
+		total += value;
+		input += " + " + value;
 	}
 	
 	public void subtract (int value) {
-		
+		total -= value;
+		input += " - " + value;
 	}
 	
 	public String toString () {
-		return "";
+		return input;
 	}
 	
 	public void clear() {
-		
+		total = 0;
 	}
 	
 }
